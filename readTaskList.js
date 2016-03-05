f30856ca38d1213446a5b9a4aee08b76d3cda816
Feliciano Santana <fsfelicianosantana@gmail.com>
2016-03-04 22:21:35 -0300

    Updates tasks.txt to tasks.json

--- readTaskList.js
+++ readTaskList.js
@@ -1,11 +1,17 @@
 var fs = require('fs');
 var path = require('path');
+var http = require("http");
 
-var filePath = path.join(__dirname, 'tarefas.txt');
+var filePath = path.join(__dirname, 'tarefas.json');
 
 fs.readFile(filePath, {encoding: 'utf-8'}, function(err, data){
     if (!err) {
-	    console.log('Tarefas a fazer: \n' + data);
+    	var tarefas = JSON.parse(data);
+    	console.log('Tarefas a fazer:');
+    	for (var i = 0; i < tarefas.length; i++) {
+    		 console.log(tarefas[i]);
+    	}
+	    
     } else {
     	console.log('Ocorreu um erro ao ler o arquivo!')
         console.log(err);
