5bb3ccc55a014fbc28d712efe33196fc202bd68f
Feliciano Santana <fsfelicianosantana@gmail.com>
2016-03-05 01:50:51 -0300

    Add validators to Task model

--- api/models/Tarefa.js
+++ api/models/Tarefa.js
@@ -1,8 +1,24 @@
 var mongoose = require('mongoose');
 
 var tarefaSchema = mongoose.Schema({
-    conteudo: String,
-    createdAt: Date,
+    conteudo: {
+    	type: String, 
+		validate: {
+		    validator: function(v) {
+		      return v !== null && v !== '';
+		    },
+		    message: 'Não é possível criar uma tarefa com o conteudo vazio!'
+	    }
+	},
+    createdAt: {
+    	type: Date,
+    	validate: {
+		    validator: function(v) {
+		      return v !== null;
+		    },
+		    message: 'Não é possível criar uma tarefa sem data de criação!'
+	    }
+	},
     finishedAt: Date,
     responsavel: { type: mongoose.Schema.Types.ObjectId, ref: 'Pessoa' }
 });
