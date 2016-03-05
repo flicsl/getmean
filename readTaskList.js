02143b69779641820c28285e568c0d78fe723b18
Feliciano Santana <fsfelicianosantana@gmail.com>
2016-03-04 22:33:26 -0300

    Add basic HTTP server for task example

--- readTaskList.js
+++ readTaskList.js
@@ -1,18 +1,35 @@
 var fs = require('fs');
 var path = require('path');
+var http = require('http');
 
 var filePath = path.join(__dirname, 'tarefas.json');
 
-fs.readFile(filePath, {encoding: 'utf-8'}, function(err, data){
-    if (!err) {
-    	var tarefas = JSON.parse(data);
-    	console.log('Tarefas a fazer:');
-    	for (var i = 0; i < tarefas.length; i++) {
-    		 console.log(tarefas[i]);
-    	}
-	    
-    } else {
-    	console.log('Ocorreu um erro ao ler o arquivo!')
-        console.log(err);
-    }
+var server = http.createServer(function (request, response) {
+	getTarefas(function (err, tarefas) {
+		if (!err) {
+			response.writeHead(200, { "Content": "application/json" });
+			response.write(JSON.stringify(tarefas));
+		}
+		else {
+			response.writeHead(500);
+		}
+		response.end();
+	});
 });
+
+server.listen(3000, function () {
+	console.log("Servidor ouvindo na porta 3000.");
+});
+
+function getTarefas (callback) {
+	fs.readFile(filePath, {encoding: 'utf-8'}, function(err, data){
+	    if (!err) {
+	    	var tarefas = JSON.parse(data);
+	    	callback(err, tarefas);
+	    } else {
+	    	console.log('Ocorreu um erro ao ler o arquivo!')
+	        console.log(err);
+	        callback(err, null);
+	    }
+	});
+}
